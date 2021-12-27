e5b8cb5e1262c145d7955279602e9cf0e5e18c6c
rabiyulfahimhasim786 <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2021-12-27 23:01:17 +0530

    Add files via upload

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    path('new/', views.new, name='new'),
+    path('', views.index, name='index'),
+    path('currencies/', views.CurrencyList.as_view()),
+    path('currenies/<int:pk>/', views.CurrencyDetail.as_view()),
+    path('countries/', views.CountryList.as_view()),
+    path('countries/<int:pk>/', views.CountryDetail.as_view()),
+    path('standards/', views.StandardList.as_view()),
+    path('standards/<int:pk>/', views.StandardDetail.as_view()),
+    path('tag/', views.TagList.as_view()),
+    path('tag/<int:pk>/', views.TagDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
